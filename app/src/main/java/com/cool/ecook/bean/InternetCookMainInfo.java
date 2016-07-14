c2940b0126eea88b26f7b14f5439cf077de92cca
ningyachao <394226258@qq.com>
2016-07-14 13:32:52 +0800

    网上厨房主界面部分

---
+++ app/src/main/java/com/cool/ecook/bean/InternetCookMainInfo.java
@@ -0,0 +1,334 @@
+package com.cool.ecook.bean;
+
+import java.util.List;
+
+/**
+ * Created by ningyachao on 2016/7/13.
+ */
+public class InternetCookMainInfo {
+
+
+    /**
+     * bannerList : [{"image":"20328387","url":"http://www.ecook.cn/ecook/viewContent.shtml?id=15034029"},{"image":"224217666","url":"http://pic.xiaochushuo.com/minisite/oneweek01/mall.ecook.cn.minisite12.html?f=ecook_share"},{"image":"224204792","url":"http://www.ecook.cn/ecook/viewContent.shtml?id=224182340"},{"image":"224157745","url":"ecook://talk?id=224156544"},{"image":"18898823","url":"http://www.ecook.cn/ecook/viewContent.shtml?id=15108956"}]
+     * buttonList : [{"imageId":"223364865","num":1,"id":1,"state":1,"title":"签到","url":""},{"imageId":"223364873","num":2,"id":2,"state":1,"title":"商城","url":"http://mall.ecook.cn/category/list?f=ecook_show_mine"},{"imageId":"223364874","num":3,"id":3,"state":1,"title":"视频","url":""},{"imageId":"223364878","num":4,"id":4,"state":1,"title":"活动","url":""}]
+     * contentList : [{"image":"224333067","collectionNum":27,"title":"全麦苏打面包","type":1,"userId":24260526,"likeNum":0,"userImage":"223322988","totalNum":"","userNick":"无忧de小时光","sortId":1591,"id":224301973,"desc":"炎热的夏天到了，我又加入了轰轰烈烈的减脂大军！说来惭愧，冬天总是馋，又懒得动，攒得一身五花肉都要留到夏天来消耗！甜点是不敢做了，长肉真的是高高的！终于尝试了这款不用发酵的方子，简直太适合我了，没有额外添加的油和糖，比花式面包减少了许多热量，一半分量的全麦粉加入，迈进了低GI食物的门槛，各种果干和果仁的加入，丰富了口感和层次！为了当做健身代餐我还加了两大勺蛋白粉，锻炼以后切一片嚼一嚼！且不说能有多少用，至少心里是非常满足的！口感软中带韧，越嚼越香，也是一款非常不错的磨牙小食！不用发酵，不用揉面，超级快手！"},{"image":"4802696","collectionNum":6974,"title":"男神张亮推荐--水煮牛肉'老公出品","type":3,"userId":1305158,"likeNum":0,"userImage":"1305159","totalNum":"","userNick":"紫滢儿","sortId":1590,"id":4802838,"desc":"材料：牛肉豆瓣酱大蒜子黄豆芽或生菜八角'�"},{"image":"224333068","collectionNum":21,"title":"现榨草莓果肉果汁","type":1,"userId":32097495,"likeNum":0,"userImage":"11998013","totalNum":"","userNick":"ALLEN","sortId":1589,"id":224290981,"desc":"这是一种健康的生活方式！"},{"image":"19236567","collectionNum":0,"title":"慵懒的周末，「早午餐」走起！","type":2,"userId":7961810,"likeNum":0,"userImage":"7962609","totalNum":"一共45菜","userNick":"爱背包的小厨子","sortId":1588,"id":11810855,"desc":"美好的周末，一觉到中午，就差醒来的一份早午餐。"},{"image":"224333076","collectionNum":77,"title":"冰糖黄桃罐头","type":1,"userId":23754959,"likeNum":0,"userImage":"223915164","totalNum":"","userNick":"妙江南","sortId":1587,"id":224321350,"desc":"冰糖黄桃罐头冰凉一夏"},{"image":"224333085","collectionNum":105,"title":"西红柿鸡蛋疙瘩汤","type":1,"userId":21128551,"likeNum":0,"userImage":"21926649","totalNum":"","userNick":"爱我就带我去吃肉","sortId":1586,"id":224321720,"desc":"简单好吃还抗饿 家里厨房最常出现的一道菜✨吃下去胃里暖暖的"},{"image":"224291067","collectionNum":104,"title":"柠香风琴土豆","type":1,"userId":12218018,"likeNum":0,"userImage":"18438319","totalNum":"","userNick":"幸福小精灵静静","sortId":1585,"id":224291154,"desc":"土豆的吃法千千万万我特别喜欢吃烤土豆，尤其是柠香风琴土豆，浓浓的土豆香混合了柠檬的酸爽让整个空气中都弥漫着诱人的味道。\n"},{"image":"4174915","collectionNum":2594,"title":"椰蓉南瓜豆沙糯米卷","type":3,"userId":4174777,"likeNum":0,"userImage":"4174787","totalNum":"","userNick":"摩卡tl","sortId":1584,"id":4174975,"desc":"材料：南瓜200克糯米粉200克红豆沙馅1袋椰蓉适量做法：南瓜去皮去籽切丁，上蒸锅蒸熟用勺压成泥状将南瓜泥放凉后按1：1左右的比例，一"},{"image":"224231601","collectionNum":139,"title":"牛奶水果布丁","type":1,"userId":6231556,"likeNum":0,"userImage":"14482500","totalNum":"","userNick":"月牙小厨","sortId":1583,"id":224287656,"desc":""},{"image":"15880001","collectionNum":0,"title":"不愿活在没有巧克力的世界","type":2,"userId":2830030,"likeNum":0,"userImage":"2830039","totalNum":"一共84菜","userNick":"糖小饼","sortId":1582,"id":9388531,"desc":"情迷巧克力。"}]
+     * desc : {"talkDesc":"晒美食，晒生活，玩活动","recipeDesc":"写菜谱能得500厨币哟"}
+     */
+
+    private DataBean data;
+    /**
+     * data : {"bannerList":[{"image":"20328387","url":"http://www.ecook.cn/ecook/viewContent.shtml?id=15034029"},{"image":"224217666","url":"http://pic.xiaochushuo.com/minisite/oneweek01/mall.ecook.cn.minisite12.html?f=ecook_share"},{"image":"224204792","url":"http://www.ecook.cn/ecook/viewContent.shtml?id=224182340"},{"image":"224157745","url":"ecook://talk?id=224156544"},{"image":"18898823","url":"http://www.ecook.cn/ecook/viewContent.shtml?id=15108956"}],"buttonList":[{"imageId":"223364865","num":1,"id":1,"state":1,"title":"签到","url":""},{"imageId":"223364873","num":2,"id":2,"state":1,"title":"商城","url":"http://mall.ecook.cn/category/list?f=ecook_show_mine"},{"imageId":"223364874","num":3,"id":3,"state":1,"title":"视频","url":""},{"imageId":"223364878","num":4,"id":4,"state":1,"title":"活动","url":""}],"contentList":[{"image":"224333067","collectionNum":27,"title":"全麦苏打面包","type":1,"userId":24260526,"likeNum":0,"userImage":"223322988","totalNum":"","userNick":"无忧de小时光","sortId":1591,"id":224301973,"desc":"炎热的夏天到了，我又加入了轰轰烈烈的减脂大军！说来惭愧，冬天总是馋，又懒得动，攒得一身五花肉都要留到夏天来消耗！甜点是不敢做了，长肉真的是高高的！终于尝试了这款不用发酵的方子，简直太适合我了，没有额外添加的油和糖，比花式面包减少了许多热量，一半分量的全麦粉加入，迈进了低GI食物的门槛，各种果干和果仁的加入，丰富了口感和层次！为了当做健身代餐我还加了两大勺蛋白粉，锻炼以后切一片嚼一嚼！且不说能有多少用，至少心里是非常满足的！口感软中带韧，越嚼越香，也是一款非常不错的磨牙小食！不用发酵，不用揉面，超级快手！"},{"image":"4802696","collectionNum":6974,"title":"男神张亮推荐--水煮牛肉'老公出品","type":3,"userId":1305158,"likeNum":0,"userImage":"1305159","totalNum":"","userNick":"紫滢儿","sortId":1590,"id":4802838,"desc":"材料：牛肉豆瓣酱大蒜子黄豆芽或生菜八角'�"},{"image":"224333068","collectionNum":21,"title":"现榨草莓果肉果汁","type":1,"userId":32097495,"likeNum":0,"userImage":"11998013","totalNum":"","userNick":"ALLEN","sortId":1589,"id":224290981,"desc":"这是一种健康的生活方式！"},{"image":"19236567","collectionNum":0,"title":"慵懒的周末，「早午餐」走起！","type":2,"userId":7961810,"likeNum":0,"userImage":"7962609","totalNum":"一共45菜","userNick":"爱背包的小厨子","sortId":1588,"id":11810855,"desc":"美好的周末，一觉到中午，就差醒来的一份早午餐。"},{"image":"224333076","collectionNum":77,"title":"冰糖黄桃罐头","type":1,"userId":23754959,"likeNum":0,"userImage":"223915164","totalNum":"","userNick":"妙江南","sortId":1587,"id":224321350,"desc":"冰糖黄桃罐头冰凉一夏"},{"image":"224333085","collectionNum":105,"title":"西红柿鸡蛋疙瘩汤","type":1,"userId":21128551,"likeNum":0,"userImage":"21926649","totalNum":"","userNick":"爱我就带我去吃肉","sortId":1586,"id":224321720,"desc":"简单好吃还抗饿 家里厨房最常出现的一道菜✨吃下去胃里暖暖的"},{"image":"224291067","collectionNum":104,"title":"柠香风琴土豆","type":1,"userId":12218018,"likeNum":0,"userImage":"18438319","totalNum":"","userNick":"幸福小精灵静静","sortId":1585,"id":224291154,"desc":"土豆的吃法千千万万我特别喜欢吃烤土豆，尤其是柠香风琴土豆，浓浓的土豆香混合了柠檬的酸爽让整个空气中都弥漫着诱人的味道。\n"},{"image":"4174915","collectionNum":2594,"title":"椰蓉南瓜豆沙糯米卷","type":3,"userId":4174777,"likeNum":0,"userImage":"4174787","totalNum":"","userNick":"摩卡tl","sortId":1584,"id":4174975,"desc":"材料：南瓜200克糯米粉200克红豆沙馅1袋椰蓉适量做法：南瓜去皮去籽切丁，上蒸锅蒸熟用勺压成泥状将南瓜泥放凉后按1：1左右的比例，一"},{"image":"224231601","collectionNum":139,"title":"牛奶水果布丁","type":1,"userId":6231556,"likeNum":0,"userImage":"14482500","totalNum":"","userNick":"月牙小厨","sortId":1583,"id":224287656,"desc":""},{"image":"15880001","collectionNum":0,"title":"不愿活在没有巧克力的世界","type":2,"userId":2830030,"likeNum":0,"userImage":"2830039","totalNum":"一共84菜","userNick":"糖小饼","sortId":1582,"id":9388531,"desc":"情迷巧克力。"}],"desc":{"talkDesc":"晒美食，晒生活，玩活动","recipeDesc":"写菜谱能得500厨币哟"}}
+     * state : 200
+     * message : 获取成功！
+     */
+
+    private String state;
+    private String message;
+
+    public DataBean getData() {
+        return data;
+    }
+
+    public void setData(DataBean data) {
+        this.data = data;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public static class DataBean {
+        /**
+         * talkDesc : 晒美食，晒生活，玩活动
+         * recipeDesc : 写菜谱能得500厨币哟
+         */
+
+        private DescBean desc;
+        /**
+         * image : 20328387
+         * url : http://www.ecook.cn/ecook/viewContent.shtml?id=15034029
+         */
+
+        private List<BannerListBean> bannerList;
+        /**
+         * imageId : 223364865
+         * num : 1
+         * id : 1
+         * state : 1
+         * title : 签到
+         * url :
+         */
+
+        private List<ButtonListBean> buttonList;
+        /**
+         * image : 224333067
+         * collectionNum : 27
+         * title : 全麦苏打面包
+         * type : 1
+         * userId : 24260526
+         * likeNum : 0
+         * userImage : 223322988
+         * totalNum :
+         * userNick : 无忧de小时光
+         * sortId : 1591
+         * id : 224301973
+         * desc : 炎热的夏天到了，我又加入了轰轰烈烈的减脂大军！说来惭愧，冬天总是馋，又懒得动，攒得一身五花肉都要留到夏天来消耗！甜点是不敢做了，长肉真的是高高的！终于尝试了这款不用发酵的方子，简直太适合我了，没有额外添加的油和糖，比花式面包减少了许多热量，一半分量的全麦粉加入，迈进了低GI食物的门槛，各种果干和果仁的加入，丰富了口感和层次！为了当做健身代餐我还加了两大勺蛋白粉，锻炼以后切一片嚼一嚼！且不说能有多少用，至少心里是非常满足的！口感软中带韧，越嚼越香，也是一款非常不错的磨牙小食！不用发酵，不用揉面，超级快手！
+         */
+
+        private List<ContentListBean> contentList;
+
+        public DescBean getDesc() {
+            return desc;
+        }
+
+        public void setDesc(DescBean desc) {
+            this.desc = desc;
+        }
+
+        public List<BannerListBean> getBannerList() {
+            return bannerList;
+        }
+
+        public void setBannerList(List<BannerListBean> bannerList) {
+            this.bannerList = bannerList;
+        }
+
+        public List<ButtonListBean> getButtonList() {
+            return buttonList;
+        }
+
+        public void setButtonList(List<ButtonListBean> buttonList) {
+            this.buttonList = buttonList;
+        }
+
+        public List<ContentListBean> getContentList() {
+            return contentList;
+        }
+
+        public void setContentList(List<ContentListBean> contentList) {
+            this.contentList = contentList;
+        }
+
+        public static class DescBean {
+            private String talkDesc;
+            private String recipeDesc;
+
+            public String getTalkDesc() {
+                return talkDesc;
+            }
+
+            public void setTalkDesc(String talkDesc) {
+                this.talkDesc = talkDesc;
+            }
+
+            public String getRecipeDesc() {
+                return recipeDesc;
+            }
+
+            public void setRecipeDesc(String recipeDesc) {
+                this.recipeDesc = recipeDesc;
+            }
+        }
+
+        public static class BannerListBean {
+            private String image;
+            private String url;
+
+            public String getImage() {
+                return image;
+            }
+
+            public void setImage(String image) {
+                this.image = image;
+            }
+
+            public String getUrl() {
+                return url;
+            }
+
+            public void setUrl(String url) {
+                this.url = url;
+            }
+        }
+
+        public static class ButtonListBean {
+            private String imageId;
+            private int num;
+            private int id;
+            private int state;
+            private String title;
+            private String url;
+
+            public String getImageId() {
+                return imageId;
+            }
+
+            public void setImageId(String imageId) {
+                this.imageId = imageId;
+            }
+
+            public int getNum() {
+                return num;
+            }
+
+            public void setNum(int num) {
+                this.num = num;
+            }
+
+            public int getId() {
+                return id;
+            }
+
+            public void setId(int id) {
+                this.id = id;
+            }
+
+            public int getState() {
+                return state;
+            }
+
+            public void setState(int state) {
+                this.state = state;
+            }
+
+            public String getTitle() {
+                return title;
+            }
+
+            public void setTitle(String title) {
+                this.title = title;
+            }
+
+            public String getUrl() {
+                return url;
+            }
+
+            public void setUrl(String url) {
+                this.url = url;
+            }
+        }
+
+        public static class ContentListBean {
+            private String image;
+            private int collectionNum;
+            private String title;
+            private int type;
+            private int userId;
+            private int likeNum;
+            private String userImage;
+            private String totalNum;
+            private String userNick;
+            private int sortId;
+            private int id;
+            private String desc;
+
+            public String getImage() {
+                return image;
+            }
+
+            public void setImage(String image) {
+                this.image = image;
+            }
+
+            public int getCollectionNum() {
+                return collectionNum;
+            }
+
+            public void setCollectionNum(int collectionNum) {
+                this.collectionNum = collectionNum;
+            }
+
+            public String getTitle() {
+                return title;
+            }
+
+            public void setTitle(String title) {
+                this.title = title;
+            }
+
+            public int getType() {
+                return type;
+            }
+
+            public void setType(int type) {
+                this.type = type;
+            }
+
+            public int getUserId() {
+                return userId;
+            }
+
+            public void setUserId(int userId) {
+                this.userId = userId;
+            }
+
+            public int getLikeNum() {
+                return likeNum;
+            }
+
+            public void setLikeNum(int likeNum) {
+                this.likeNum = likeNum;
+            }
+
+            public String getUserImage() {
+                return userImage;
+            }
+
+            public void setUserImage(String userImage) {
+                this.userImage = userImage;
+            }
+
+            public String getTotalNum() {
+                return totalNum;
+            }
+
+            public void setTotalNum(String totalNum) {
+                this.totalNum = totalNum;
+            }
+
+            public String getUserNick() {
+                return userNick;
+            }
+
+            public void setUserNick(String userNick) {
+                this.userNick = userNick;
+            }
+
+            public int getSortId() {
+                return sortId;
+            }
+
+            public void setSortId(int sortId) {
+                this.sortId = sortId;
+            }
+
+            public int getId() {
+                return id;
+            }
+
+            public void setId(int id) {
+                this.id = id;
+            }
+
+            public String getDesc() {
+                return desc;
+            }
+
+            public void setDesc(String desc) {
+                this.desc = desc;
+            }
+        }
+    }
+}
--- app/src/main/java/com/cool/ecook/fragment/InterCookFragment.java
+++ app/src/main/java/com/cool/ecook/fragment/InterCookFragment.java
@@ -1,13 +1,31 @@
 package com.cool.ecook.fragment;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import com.androidxx.yangjw.httplibrary.IOKCallBack;
+import com.androidxx.yangjw.httplibrary.OkHttpTool;
+import com.bigkoo.convenientbanner.ConvenientBanner;
+import com.bigkoo.convenientbanner.holder.CBViewHolderCreator;
+import com.bigkoo.convenientbanner.holder.Holder;
+import com.bumptech.glide.Glide;
 import com.cool.ecook.R;
+import com.cool.ecook.adapter.InternetCookMainListViewAdapter;
+import com.cool.ecook.bean.InternetCookMainInfo;
+import com.google.gson.Gson;
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 网上厨房
@@ -15,6 +33,24 @@ import com.cool.ecook.R;
  */
 public class InterCookFragment extends Fragment {
 
+
+    private View view;
+    private PullToRefreshListView prflv;
+    private ListView listView;
+    private View header_view;
+    private ConvenientBanner convenientBanner;
+    private List<InternetCookMainInfo.DataBean.BannerListBean> mlist = new ArrayList<>();
+    private List<InternetCookMainInfo.DataBean.ButtonListBean> nlist = new ArrayList<>();
+    private List<InternetCookMainInfo.DataBean.ContentListBean> plist = new ArrayList<>();
+    private ImageView imageViewSign;
+    private ImageView imageViewShopping;
+    private ImageView imageViewVideo;
+    private ImageView imageViewActivity;
+    private TextView textViewSign;
+    private TextView textViewShopping;
+    private TextView textViewVideo;
+    private TextView textViewActivity;
+    private InternetCookMainListViewAdapter adapter;
 
     public InterCookFragment() {
         // Required empty public constructor
@@ -33,7 +69,137 @@ public class InterCookFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inter_cook, container, false);
+
+         view =inflater.inflate(R.layout.fragment_inter_cook, container, false);
+         prflv = (PullToRefreshListView) view.findViewById(R.id.pull_to_fresh_list_view);
+         prflv.setMode(PullToRefreshBase.Mode.BOTH);
+         listView = prflv.getRefreshableView();
+        inintHeaderView();
+
+        initAdapter();
+
+        return view;
     }
 
+    private void initAdapter() {
+
+        listView.setDivider(null);
+
+        adapter = new InternetCookMainListViewAdapter(getActivity(),R.layout.inter_cook_list_item_view,plist);
+
+        listView.setAdapter(adapter);
+
+    }
+
+    /*
+     *初始化头部视图
+     */
+    private void inintHeaderView() {
+
+        header_view =LayoutInflater.from(getActivity()).inflate(R.layout.inter_cook_list_head_view, null);
+
+        convenientBanner = (ConvenientBanner) header_view.findViewById(R.id.internet_cook_ad);
+
+        imageViewSign = (ImageView) header_view.findViewById(R.id.sign);
+        imageViewShopping = (ImageView) header_view.findViewById(R.id.shopping);
+        imageViewVideo = (ImageView) header_view.findViewById(R.id.video);
+        imageViewActivity = (ImageView) header_view.findViewById(R.id.activity);
+
+        textViewSign = (TextView) header_view.findViewById(R.id.sign_tv);
+        textViewShopping = (TextView) header_view.findViewById(R.id.shopping_tv);
+        textViewVideo = (TextView) header_view.findViewById(R.id.video_tv);
+        textViewActivity = (TextView) header_view.findViewById(R.id.activity_tv);
+        setupBanner();
+        loadBannerDatas();
+        
+    }
+    /*
+     *初始化banner数据
+     */
+    private void loadBannerDatas() {
+        OkHttpTool.newInstance().start("http://api.ecook.cn/public/get124Homedata.shtml").callback(new IOKCallBack() {
+
+            @Override
+            public void success(String result) {
+                Gson gson = new Gson();
+                InternetCookMainInfo  internetCookMainInfo = gson.fromJson(result,InternetCookMainInfo.class);
+                if (!mlist.isEmpty()){
+                    return;
+                }
+                mlist.addAll(internetCookMainInfo.getData().getBannerList());
+                convenientBanner.getViewPager().getAdapter().notifyDataSetChanged();
+                listView.addHeaderView(header_view);
+
+                nlist.addAll(internetCookMainInfo.getData().getButtonList());
+
+                buttonListSet(nlist);
+
+                plist.addAll(internetCookMainInfo.getData().getContentList());
+
+                adapter.notifyDataSetChanged();
+
+
+
+            }
+        });
+
+    }
+    /*
+     *对主界面的四个Button进行设置
+     */
+
+    private void buttonListSet(List<InternetCookMainInfo.DataBean.ButtonListBean> nlist) {
+        String url = "http://pic.ecook.cn/web/"+nlist.get(0).getImageId()+".jpg!s1";
+        Glide.with(getActivity()).load(url).into(imageViewSign);
+        textViewSign.setText(nlist.get(0).getTitle());
+
+        String url1 = "http://pic.ecook.cn/web/"+nlist.get(1).getImageId()+".jpg!s1";
+        Glide.with(getActivity()).load(url1).into(imageViewShopping);
+        textViewShopping.setText(nlist.get(1).getTitle());
+
+        String url2 = "http://pic.ecook.cn/web/"+nlist.get(2).getImageId()+".jpg!s1";
+        Glide.with(getActivity()).load(url2).into(imageViewVideo);
+        textViewVideo.setText(nlist.get(2).getTitle());
+
+        String url3 = "http://pic.ecook.cn/web/"+nlist.get(3).getImageId()+".jpg!s1";
+        Glide.with(getActivity()).load(url3).into(imageViewActivity);
+        textViewActivity.setText(nlist.get(3).getTitle());
+
+
+    }
+
+    /*
+     *初始化convenietBanner
+     */
+    private void setupBanner() {
+
+        convenientBanner.setPages(new CBViewHolderCreator<NetImageViewHolder>() {
+            @Override
+            public NetImageViewHolder createHolder() {
+                return new NetImageViewHolder();
+            }
+        },mlist).startTurning(5000);
+
+
+    }
+
+    /*
+     *convenietBanner的Holder类
+     */
+class  NetImageViewHolder implements Holder<InternetCookMainInfo.DataBean.BannerListBean>{
+
+        private ImageView imageview;
+        @Override
+        public View createView(Context context) {
+        imageview = new ImageView(context);
+        imageview.setScaleType(ImageView.ScaleType.FIT_XY);
+            return imageview;
+        }
+
+        @Override
+        public void UpdateUI(Context context, int position, InternetCookMainInfo.DataBean.BannerListBean data) {
+            String url = "http://pic.ecook.cn/web/"+data.getImage()+".jpg";
+            Glide.with(getActivity()).load(url).into(imageview);
+        }
+    }
 }
