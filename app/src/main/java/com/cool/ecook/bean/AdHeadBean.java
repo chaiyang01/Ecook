eaf5d6d381c9dddc06acad738450cc7478ecc4b2
hbsyx456 <398923496@qq.com>
2016-07-20 10:07:32 +0800

    第三页面广场的广告的实体类

---
+++ app/src/main/java/com/cool/ecook/bean/AdHeadBean.java
@@ -0,0 +1,32 @@
+package com.cool.ecook.bean;
+
+/**
+ * 广场头部广告的详情图片跟详情介绍
+ * Created by lenovo on 2016/7/18.
+ */
+public class AdHeadBean {
+
+    /**
+     * imageid : 224363505
+     * description : 与精致细腻的法餐，热情随和的意大利美食以及性冷淡的北欧菜相比，德国菜，以奔放豪爽闻名于世~这周末网厨带你体验德国手工啤酒、德国脆烤猪肘等大口吃肉大口喝酒的德系菜！
+     */
+
+    private String imageid;
+    private String description;
+
+    public String getImageid() {
+        return imageid;
+    }
+
+    public void setImageid(String imageid) {
+        this.imageid = imageid;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
